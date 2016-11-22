90399248cf20467a1274d561fd6331a154aa933f
zhengwen <mail.zhengwen@gmail.com>
2016-11-22 11:01:20 +0800

    修改项目文件目录

---
+++ src/JourneyDiary.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JourneyDiary.Web.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region 全局异常捕获
+
+        /// <summary>
+        /// 全局异常捕获测试
+        /// </summary>
+        public ActionResult TestException()
+        {
+            throw new Exception("test ception");
+        }
+
+        #endregion
+
+        #region 使用分布视图加载数据
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult RenderNews(int pageIndex = 1, int pageSize = 10)
+        {
+
+            return PartialView();
+        }
+
+        #endregion 
+    }
+}
