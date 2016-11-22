1d7edbae9ab881c4cbeb1c8fff0c29be1905e34c
zhengwen <mail.zhengwen@gmail.com>
2016-11-22 15:42:54 +0800

    增加部署代码

---
+++ build/deploy.bat
@@ -0,0 +1,4 @@
+net use \\192.168.1.184\data /user:John a123456@123
+Robocopy C:\Users\John\Desktop\mysource\JourneyDiary\release\_PublishedWebsites\JourneyDiary.Web \\192.168.1.184\data /e
+net use \\192.168.1.184\data /delete
+pause
