1461817cc1641de3b629084a516687afaeb54478
zhengwen <mail.zhengwen@gmail.com>
2017-04-06 12:15:22 +0800

    修改自动发布文件

--- build/build.bat
+++ build/build.bat
@@ -1,2 +1,3 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.msbuild
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"  ..\Source\JourneyDiary.Web\JourneyDiary.Web.csproj /t:Rebuild /p:Configuration=Release /p:PublishProfile=JourneyDiary.Web.Publish.pubxml /p:DeployOnBuild=true
+
 pause
